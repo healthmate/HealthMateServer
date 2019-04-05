5ee293131c9a7802723c1e83d69d8bb201504d7f
coderiddles <godspoweropara37@gmail.com>
2019-04-05 15:46:45 +0000

    resolve

--- manage.py
+++ manage.py
@@ -2,12 +2,7 @@ from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from app import app, db, model
 from app.model import User, Post, Community
-import unittest
-import coverage
-import os
-import forgery_py as faker
-from random import randint
-from sqlalchemy.exc import IntegrityError
+
 
 # Initializing the manager
 manager = Manager(app)
@@ -19,16 +14,7 @@ migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
 
 # Test coverage configuration
-"""COV = coverage.coverage(
-    branch=True,
-    include='app/*',
-    omit=[
-        'app/auth/__init__.py',
-        'app/post/__init__.py',
-        'app/community/__init__.py'
-    ]
-)
-COV.start()"""
+
 @manager.command
 def dummy():
     # Create a user if they do not exist.
